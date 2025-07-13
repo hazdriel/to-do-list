493be21207ca95a34829da4495574a24a2dbc5d8
Cgabryel0 <cgabryel0@gmail.com>
2025-07-13 00:58:37 -0300

    Update Menu.java

--- view/Menu.java
+++ view/Menu.java
@@ -1,5 +1,22 @@
 package view;
 
+import java.util.List;
+
+import model.Usuario;
+
 public class Menu {
   
+    public void exibirListaUsuarios(List<Usuario> usuarios) {
+        if (usuarios.isEmpty()) {
+            System.out.println("Nenhum usuário cadastrado ainda!");
+            return;
+        }
+        System.out.println("  Lista de Usuários  ");
+        for (Usuario usuario : usuarios) {
+            System.out.println(usuario);
+        }
+        System.out.println(" FIM DA LISTA DE USUÁRIOS!! ");
+    }
+
+    
 }
