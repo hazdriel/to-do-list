3539bdb72b88bc48ad124ed59886e2fede3ef246
Cauã Modesto <cauacm6@gmail.com>
2025-08-31 21:50:29 -0300

    Refatoração com pequenas correções

--- dados/PersistenciaArquivo.java
+++ dados/PersistenciaArquivo.java
@@ -4,8 +4,8 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
-// Implementação de persistência usando arquivos .dat
-public class PersistenciaArquivo<T> implements IPersistencia<T> {
+// Classe de persistência usando arquivos .dat
+public class PersistenciaArquivo<T> {
     
     private final String nomeArquivo;
     private final String caminhoCompleto;
@@ -22,18 +22,15 @@ public class PersistenciaArquivo<T> implements IPersistencia<T> {
         }
     }
     
-    @Override
     public void salvar(List<T> entidades) throws PersistenciaException {
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(caminhoCompleto))) {
             oos.writeObject(entidades);
-            System.out.println("Dados salvos com sucesso em: " + caminhoCompleto);
             
         } catch (IOException e) {
             throw new PersistenciaException("Erro ao salvar dados em " + caminhoCompleto, e);
         }
     }
     
-    @Override
     @SuppressWarnings("unchecked")
     public List<T> carregar() throws PersistenciaException {
         if (!arquivoExiste()) {
@@ -42,7 +39,6 @@ public class PersistenciaArquivo<T> implements IPersistencia<T> {
         
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(caminhoCompleto))) {
             List<T> entidades = (List<T>) ois.readObject();
-            System.out.println("Dados carregados com sucesso de: " + caminhoCompleto);
             return entidades;
             
         } catch (IOException | ClassNotFoundException e) {
@@ -50,19 +46,15 @@ public class PersistenciaArquivo<T> implements IPersistencia<T> {
         }
     }
     
-    @Override
     public boolean arquivoExiste() {
         return new File(caminhoCompleto).exists();
     }
     
-    @Override
     public void limparDados() throws PersistenciaException {
         try {
             File arquivo = new File(caminhoCompleto);
             if (arquivo.exists()) {
-                if (arquivo.delete()) {
-                    System.out.println("Arquivo " + caminhoCompleto + " removido com sucesso");
-                } else {
+                if (!arquivo.delete()) {
                     throw new PersistenciaException("Não foi possível remover o arquivo " + caminhoCompleto);
                 }
             }
@@ -71,3 +63,4 @@ public class PersistenciaArquivo<T> implements IPersistencia<T> {
         }
     }
 }
+
